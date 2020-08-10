546b91b6a31c9605dafcb1c8fef242e9343e93ab
mjd3 <michael.danielczuk@gmail.com>
2020-08-09 22:47:40 -0700

    initial changes, working quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,15 +1,13 @@
 var counter = 0;
 var right = 0;
 var questions = [
-    ["Where did Maya and Keshal meet?", "Spring break, Miami - cliché, we know", "At Columbia University, obviously", "Through a friend who set them up - Hi, Friend! You know who you are ;)", "In the one Anthropology class Keshal took while at Columbia. It was called 'Magic, Witchcraft and Religion'. He thought it 'sounded cool'.", "1"],
-    ["How long have they been together?", "6 years", "7 years", "8 years", "An eternity. Seriously, what took them so long?", "4"],
-    ["How long were they in a long distance relationship?", "They were in a long distance relationship??", "3 years", "4 years", "5 years", "4"],
-    ["Where did they get engaged?", "Miami, Florida - where it all began", "Lisbon, Portugal - Maya thought it was a vacation with Keshal's family", "Boston, Massachusetts - on a random Sunday, with Otis Redding on the record player, over brunch", "New York City, New York - it was a full on flash mob", "2"],
-    ["Who is the bride-zilla between the two?", "Keshal", "Maya", "1"],
-    ["What is their preferred term of endearment?", "Boo", "Babe", "Hon", "Dear", "1"],
-    ["Which of these is a nickname Maya has for Keshal?", "Schmoomoo", "Boo Radley", "KP", "Keshal Facial", "All of the above", "5"],
-    ["Which of these is a nickname Keshal has for Maya? ", "Clown", "Ma", "Booface", "Garfield", "All of the above", "5"],
-    ["Over the years, Maya and Keshal have had a lot of adventures together. What <i>haven't</i> they done together?", "Climbed a volcano", "Scuba dived in fresh water caves", "Taken a woodworking class", "Gone to a music festival", "All of the above", "3"]
+    ["How and where did Katie and Mike meet?", "At Princeton orientation", "In the Catskills", "On a backpacking trip", "All of the above", "4"],
+    ["For the first six months, we disagreed over our anniversary date – why?", "Katie counted our anniversary from our first kiss and Mike counted it from our first date", "Mike thought our anniversary was September 31st and Katie believed it was October 1st – check a calendar to see who won this one", "Anniversaries are a social construct", "We both forgot the day and couldn’t decide which date to use as a placeholder", "2"],
+    ["While dating in college, how often did Katie accidentally lock Mike out of his own dorm room?", "Never", "Maybe just one time", "Twice but who's counting?", "Tbh we lost track", "3"],
+    ["In New York, what was our favorite Sunday morning routine?", "Takeout burritos with Mike’s roommates", "Bagel runs at Tompkins Square", "Eggs and bacon, obviously", "What morning? Sleeping past 2 pm", "1"],
+    ["In the year and a half that we were long distance, what was our longest time zone difference?", "3 hours", "2 hours", "7 hours", "9 hours", "4"],
+    ["Approximately how many miles have we backpacked together?", "Around 100 miles", "Around 200 miles", "Around 300 miles", "Idk, but check Mike’s Strava for future updates", "4"],
+    ["Mike proposed in Berkeley in August 2019 – which of these things were NOT a part of the experience?", "A trail around sunset", "Katie getting lost and almost not arriving", "A casual long run", "Views of the Bay", "3"],
 ];
 
 $(document).ready(function () {
@@ -30,21 +28,21 @@ $(document).ready(function () {
     });
 
     function nextQuestion() {
-        document.getElementById("progress").value += 12 
+        document.getElementById("progress").value += 100 / questions.length 
         counter = counter + 1;
-        if (counter > 8) {
+        if (counter >= questions.length) {
             $('#a1, #a2, #a3, #a4, #a5, #question').fadeOut("slow", function () {
-                if (right == 9){
-                    var result = $("<div id='question' class='field is-size-4'><strong class='has-text-success'>You got all " + right +"/9 right.</strong></br><div class='is-size-5'> Wow! You got them all right! There's no way you're not a stalker. Not that you need it, but scroll down to read the full story!</div></div>").hide();
+                if (right == questions.length){
+                    var result = $(`<div id='question' class='field is-size-4'><strong class='has-text-success'>You got all ${right}/${questions.length} right.</strong></br><div class='is-size-5'> Wow! You got them all right! There's no way you're not a stalker. Not that you need it, but scroll down to read the full story!</div></div>`).hide();
                 }
-                else if (right >= 7){
-                    var result = $("<div id='question' class='field is-size-4'><strong class='has-text-success'>You got " + right +"/9 right.</strong></br><div class='is-size-5'> Pretty good! You must be quite close to Maya and Keshal... or you're a stalker. Scroll down for the full story!</div></div>").hide();
+                else if (right == questions.length - 1){
+                    var result = $(`<div id='question' class='field is-size-4'><strong class='has-text-success'>You got ${right}/${questions.length} right.</strong></br><div class='is-size-5'> Pretty good! You must be quite close to Katie and Mike... or you're a stalker. Scroll down for the full story!</div></div>`).hide();
                 }
-                else if (right < 7 && right >= 3){
-                    var result = $("<div id='question' class='field is-size-4'><strong class='orange'>You got " + right +"/9 right.</strong></br><div class='is-size-5'> You've got some work to do! Scroll down and read up...</div></div>").hide();
+                else if (right < questions.length - 1 && right >= 2){
+                    var result = $(`<div id='question' class='field is-size-4'><strong class='orange'>You got ${right}/${questions.length} right.</strong></br><div class='is-size-5'> You've got some work to do! Scroll down and read up...</div></div>`).hide();
                 }
                 else {
-                    var result = $("<div id='question' class='field is-size-4'><strong class='has-text-danger'>You got " + right +"/9 right.</strong></br><div class='is-size-5'> Wow you did terribly! Do you even know Maya and Keshal!? Scroll down and take notes...</div></div>").hide();
+                    var result = $(`<div id='question' class='field is-size-4'><strong class='has-text-danger'>You got ${right}/${questions.length} right.</strong></br><div class='is-size-5'> Wow you did terribly! Do you even know Katie and Mike!? Scroll down and take notes...</div></div>`).hide();
                 }
                 $('#couple-20').replaceWith('<div id="couple-20" class="column is-4 is-offset-1"><p class="title is-2 "><span class="rsvp-label">Your Results</span></p></div>');
                 $('#question').replaceWith(result);
@@ -59,85 +57,35 @@ $(document).ready(function () {
             $(".false").css("color", "black");
 
             $('#question').fadeOut("slow", function () {
-                var newQ = $("<div id='question' class='field'><strong>Question " + (counter + 1) + "/9</strong><label id='real-question' class='label is-size-5'>" + questions[counter][0] + "</label ></div >").hide();
+                var newQ = $("<div id='question' class='field'><strong>Question " + (counter + 1) + "/" + questions.length + "</strong><label id='real-question' class='label is-size-5'>" + questions[counter][0] + "</label ></div >").hide();
                 $(this).replaceWith(newQ);
                 $('#question').fadeIn("slow");
             });
 
             var numAnswers = questions[counter].length - 2;
             var correctAnswer = questions[counter][numAnswers + 1];
+            console.log(correctAnswer)
 
-            if (numAnswers == 4) {
-                $('#a1').fadeOut("slow", function () {
-                    var newa = $("<p id='a1' class='control'><a class='box " + (correctAnswer == 1) + "'>" + questions[counter][1] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a1').fadeIn("slow");
-                });
-                $('#a2').fadeOut("slow", function () {
-                    newa = $("<p id='a2' class='control'><a class='box " + (correctAnswer == 2) + "'>" + questions[counter][2] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a2').fadeIn("slow");
-                });
-                $('#a3').fadeOut("slow", function () {
-                    newa = $("<p id='a3' class='control'><a class='box " + (correctAnswer == 3) + "'>" + questions[counter][3] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a3').fadeIn("slow");
-                });
-                $('#a4').fadeOut("slow", function () {
-                    newa = $("<p id='a4' class='control'><a class='box " + (correctAnswer == 4) + "'>" + questions[counter][4] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a4').fadeIn("slow");
-                });
-            }
-            else if (numAnswers == 5) {
-                $('#a1').fadeOut("slow", function () {
-                    var newa = $("<p id='a1' class='control'><a class='box " + (correctAnswer == 2) + "'>" + questions[counter][2] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a1').fadeIn("slow");
-                });
-                $('#a2').fadeOut("slow", function () {
-                    newa = $("<p id='a2' class='control'><a class='box " + (correctAnswer == 3) + "'>" + questions[counter][3] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a2').fadeIn("slow");
-                });
-                $('#a3').fadeOut("slow", function () {
-                    newa = $("<p id='a3' class='control'><a class='box " + (correctAnswer == 4) + "'>" + questions[counter][4] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a3').fadeIn("slow");
-                });
-                $('#a4').fadeOut("slow", function () {
-                    newa = $("<p id='a4' class='control'><a class='box " + (correctAnswer == 5) + "'>" + questions[counter][5] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a4').fadeIn("slow");
-                });
-                $('#a5').fadeOut("slow", function () {
-                    newa = $("<p id='a5' class='control'><a class='box " + (correctAnswer == 6) + "'>" + questions[counter][1] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a5').fadeIn("slow");
-                });
-            }
-            else {
-                $('#a1').fadeOut("slow", function () {
-                    var newa = $("<p id='a1' class='control'><a class='box " + (correctAnswer == 1) + "'>" + questions[counter][1] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a1').fadeIn("slow");
-                });
-                $('#a2').fadeOut("slow", function () {
-                    newa = $("<p id='a2' class='control'><a class='box " + (correctAnswer == 2) + "'>" + questions[counter][2] + "</a></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a2').fadeIn("slow");
-                });
-                $('#a3').fadeOut("slow", function () {
-                    newa = $("<p id='a3' class='control'></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a3').fadeIn("slow");
-                });
-                $('#a4').fadeOut("slow", function () {
-                    newa = $("<p id='a4' class='control'></p>").hide();
-                    $(this).replaceWith(newa);
-                    $('#a4').fadeIn("slow");
-                });
-            }
+            $('#a1').fadeOut("slow", function () {
+                var newa = $(`<p id='a1' class='control'><a class='box ${correctAnswer == 1} '> ${questions[counter][1]}</a></p>`).hide();
+                $(this).replaceWith(newa);
+                $('#a1').fadeIn("slow");
+            });
+            $('#a2').fadeOut("slow", function () {
+                newa = $(`<p id='a2' class='control'><a class='box ${correctAnswer == 2} '> ${questions[counter][2]}</a></p>`).hide();
+                $(this).replaceWith(newa);
+                $('#a2').fadeIn("slow");
+            });
+            $('#a3').fadeOut("slow", function () {
+                newa = $(`<p id='a3' class='control'><a class='box ${correctAnswer == 3} '> ${questions[counter][3]}</a></p>`).hide();
+                $(this).replaceWith(newa);
+                $('#a3').fadeIn("slow");
+            });
+            $('#a4').fadeOut("slow", function () {
+                newa = $(`<p id='a4' class='control'><a class='box ${correctAnswer == 4} '> ${questions[counter][4]}</a></p>`).hide();
+                $(this).replaceWith(newa);
+                $('#a4').fadeIn("slow");
+            });
 
         }
     }
